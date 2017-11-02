13ae292b8fe90e2f11591439c33c6061aaccbcb9
longsky19856 <lushichao8@163.com>
2017-11-02 08:56:57 +0800

    实现透传功能; 1、通过按键S1设置发送和接收模式 2、在发送模式下，串口接收到的数据直接通过SI4432发送； 3、在接收模式下，SI4432收到的数据，通过串口发送出来；

--- eBox_STM32F1/user/main.cpp
+++ eBox_STM32F1/user/main.cpp
@@ -12,11 +12,14 @@ Copyright 2015 shentq. All Rights Reserved.
 #include "wstring.h"
 #include "bsp.h"
 #include "si4432.h"
+#include "button.h"
 
 Si4432	rf(&PG8,&PG6,&PG7,&spi2);
-Button btn(&PA8, 0);
+Button btn(&PE4, 1);
 
 Led     led_net(&PB5,0);
+Timer timer1(TIM1);
+Timer timer2(TIM2);
 
 
 ///////////////////////////////////
@@ -26,23 +29,27 @@ uint32_t    count = 0;
 
 uint8_t rbuf[512];
 uint8_t wbuf[512];
+uint8_t uart1_rx_buf[512];
+uint8_t uart1_rx_buf_len=0;
+uint8_t uart1_rx_buf_c[512];
+uint8_t uart1_rx_buf_c_len=0;
 uint8_t module_si4432=0;
-
+uint8_t timer1_flag=0;
+uint8_t timer2_flag=0;
 
 //????????
-void RF_TxData(uint8_t protno)
+void RF_TxData(uint8_t *protno,uint8_t len_t)
 {
-	uint8_t TxBuf_RF[32];
+	uint8_t TxBuf_RF[200];
 	//?????? TxBuf_RF??
-	TxBuf_RF[0] = 0x5A;			//??
-	TxBuf_RF[1] = 0xA5;			//??
-	TxBuf_RF[2] = 6;	 	//???
-	TxBuf_RF[3] = protno;			//
-	TxBuf_RF[4] = 0xAA;			//??
-	TxBuf_RF[4] = 0xBB;			//??
+	
+  for(int i=0;i<len_t;i++)
+  {
+    TxBuf_RF[i]=protno[i];
+  }
 
 	//FIFO??
-	rf.write(TxBuf_RF,5+len);
+	rf.write(TxBuf_RF,len_t);
 
 	//?????????,??????????????????????????CRC????????
 	rf.SpiWriteRegister(0x05, 0x03);
@@ -54,20 +61,38 @@ void RF_TxData(uint8_t protno)
 void rx_event()
 {
     uint8_t c;
-    c = uart1.read();
-    uart1.write(c);
-    if(c==0xa5)
+    
+    timer2.start();
+    c = uart1.read(); 
+    //uart1.write(c);
+    if(uart1_rx_buf_len<200)
     {
-      module_si4432=1;
-      led_net.on();
+      uart1_rx_buf[uart1_rx_buf_len]=c;
     }
-    else if(c==0xaa)
-    {
-      module_si4432=0;
-      led_net.off();
-    }
+    uart1_rx_buf_len++;
     
     
+    
+    
+}
+void t1it()
+{
+    timer1_flag=1; 
+    //uart1.printf("timer1 event!\r\n");
+}
+void t2it()
+{
+    timer2_flag=1;
+    timer2.stop();
+    //uart1.printf("timer2 event!,%d\r\n",uart1_rx_buf_len);
+    
+      for(int jj=0;jj<=uart1_rx_buf_len;jj++)
+        {
+          uart1_rx_buf_c[jj]=uart1_rx_buf[jj];
+        }
+    
+    uart1_rx_buf_c_len=uart1_rx_buf_len;
+    uart1_rx_buf_len=0;
 }
 void setup()
 {
@@ -80,6 +105,16 @@ void setup()
 	rf.begin(2);
 	btn.begin();
 	led_net.begin();
+  //
+  timer1.begin(1);    
+  timer1.attach(t1it);
+  timer1.interrupt(ENABLE);
+  timer1.start();
+  //
+  timer2.begin(100);    
+  timer2.attach(t2it);
+  timer2.interrupt(ENABLE);
+  //timer2.start();
 	
 
 }
@@ -105,35 +140,53 @@ int main(void)
 	while(1)
 	{
 		
-    
+    btn.loop();
+    if(btn.release())
+        {
+            
+          if( 1==module_si4432 )
+            {
+              module_si4432=0;
+              led_net.off();
+              uart1.printf("\r\nclick event!,rx_module");
+            }
+            else
+            {
+              module_si4432=1;
+              led_net.on();
+              uart1.printf("\r\nclick event!£¬tx_module");
+            }
+            uart1.printf("click event!\r\n");
+            
+            
+            
+        }
+        
     //send 0xff
-    
-    
-    if (module_si4432==1)
+    if(timer2_flag==1&&module_si4432==1&&timer1_flag==1)
     {
-    tmp=rf.SpiReadRegister(0X01);
-    uart1.printf("reg01 is %x\n",tmp);
-    tmp=rf.read_rssi();
-    uart1.printf("RSSI is %x\n",tmp);
-    RF_TxData(0xff);
-    uart1.printf("SEND DATA is %x\n",0XFF);
-    delay_ms(1000);
-    }
+      timer2_flag=0;      
+      timer1_flag=0;
+      RF_TxData(uart1_rx_buf_c,uart1_rx_buf_c_len);
+    } 
+    
+    
 		//????????
 		if(rf.read_irq() == 0)
 		{
 			//??????????,????????,???????
 			rf.set_idle_mode();
 			uint8_t read_len = rf.read(rbuf);
-			uart1.printf("read data length:%d\n",read_len);
-      uart1.printf("read data :");
+			//uart1.printf("read data length:%d\n",read_len);
+      //uart1.printf("read data :");
       for(i=0;i<read_len;i++)
       {
-        uart1.printf("%x,",rbuf[i]);
+        uart1.write(rbuf[i]);
       }
-      uart1.printf("\n");
+      //uart1.printf("\n");
       rf.set_rx_mode();
 		}
+    
     
 	}
 }
